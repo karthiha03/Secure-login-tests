e6060ebbc2e4d037470ef1722b5dc45e9c6e5530
karthiha03 <142159264+karthiha03@users.noreply.github.com>
2025-05-15 11:02:09 +0530

    LoginPage.java

---
+++ LoginPage.java
@@ -0,0 +1,35 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+
+    @FindBy(id = "username")
+    WebElement usernameField;
+
+    @FindBy(id = "password")
+    WebElement passwordField;
+
+    @FindBy(id = "loginBtn")
+    WebElement loginButton;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void login(String username, String password) {
+        usernameField.clear();
+        usernameField.sendKeys(username);
+
+        passwordField.clear();
+        passwordField.sendKeys(password);
+
+        loginButton.click();
+    }
+}
